c792f9d6e3e937c564373234417f2491c160387a
riddh1 <67861922+riddh1@users.noreply.github.com>
2020-07-05 17:16:02 +0530

    Add files via upload

---
+++ piano.js
@@ -0,0 +1,16 @@
+$("button.M").click(function () {
+  var audio= new Audio("crash.mp3");
+  audio.play();
+});
+$("button.N").click(function () {
+  var audio= new Audio("beat 1.mp3");
+  audio.play();
+});
+$("button.J").click(function () {
+  var audio= new Audio("beat2.mp3");
+  audio.play();
+});
+$("button.K").click(function () {
+  var audio= new Audio("beat3.mp3");
+  audio.play();
+});
